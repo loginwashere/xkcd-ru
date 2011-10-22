d51692b251e1735ddc453f3485ca468b4df5ad81
Dmitry Savchenko <login.was.here@gmail.com>
2011-10-23 01:48:23 +0300

    Fix for addinf pagination

--- application/modules/comics/controllers/IndexController.php
+++ application/modules/comics/controllers/IndexController.php
@@ -15,12 +15,20 @@ class Comics_IndexController extends Zend_Controller_Action
             //throw new Zend_Controller_Action_Exeption('Page not found');
             $id = 1;
         }
-
         $model = new Comics_Model_DbTable_Comics;
+
+        $adapter = new Zend_Paginator_Adapter_DbSelect($model->select()->from('comics'));
+        $paginator = new Zend_Paginator($adapter);
+        $paginator->setItemCountPerPage(1);
+        $paginator->setCurrentPageNumber($id);
+
+        $this->view->paginator = $paginator;
+        /*
+
         $this->view->comics = $model->find($id)->current();
         if (! $this->view->comics) {
             throw new Zend_Controller_Action_Exeption('Comics not found');
-        }
+        }*/
     }
 
 
