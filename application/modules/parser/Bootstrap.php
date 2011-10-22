4a0db8075179e16c6f4ae648f3347bf6e3776181
Dmitry Savchenko <login.was.here@gmail.com>
2011-10-22 21:26:34 +0300

    Added frame for parser module

---
+++ application/modules/parser/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bootstrap Parser Module
+ *
+ * @category   Application
+ * @package    Parser
+ * @subpackage Bootstrap
+ */
+class Parser_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
