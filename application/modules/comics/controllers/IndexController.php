4227b463a3c6f92ed0e8f4cc747f6727e380da34
Dmitriy Savchenko <login.was.here@gmail.com>
2011-11-26 21:30:54 +0300

    Pagination creation moved from controller to model

--- application/modules/comics/controllers/IndexController.php
+++ application/modules/comics/controllers/IndexController.php
@@ -6,31 +6,15 @@ class Comics_IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $ajaxContext = $this->_helper->getHelper('AjaxContext');
+        $ajaxContext->addActionContext('index', 'json')
+                    ->initContext();        
     }
 
     public function indexAction()
     {
         $id = $this->_getParam('id', null, 'int');
-        if (!$id) {
-            //throw new Zend_Controller_Action_Exeption('Page not found');
-            $id = 1;
-        }
-        $model = new Comics_Model_DbTable_Comics;
-
-        $adapter = new Zend_Paginator_Adapter_DbSelect($model->select()->from('comics'));
-        $paginator = new Zend_Paginator($adapter);
-        $paginator->setItemCountPerPage(1);
-        $paginator->setCurrentPageNumber($id);
-
-        $this->view->paginator = $paginator;
-        /*
-
-        $this->view->comics = $model->find($id)->current();
-        if (! $this->view->comics) {
-            throw new Zend_Controller_Action_Exeption('Comics not found');
-        }*/
+        $model = new Comics_Model_Comics();
+        $this->view->paginator = $model->getComics($id);
     }
-
-
 }
-
--- application/modules/comics/models/Comics.php
+++ application/modules/comics/models/Comics.php
@@ -2,5 +2,19 @@
 
 class Comics_Model_Comics
 {
+    const DEFAULT_COMICS = 1;
+    const COMICS_PER_PAGE = 1;
     
+    public function getComics($id = self::DEFAULT_COMICS)
+    {
+        $model = new Comics_Model_DbTable_Comics;
+        $select = $model->select()
+                ->from('comics')
+                ->limitPage($id, self::COMICS_PER_PAGE);
+        $adapter = new Zend_Paginator_Adapter_DbSelect($select);
+        $paginator = new Zend_Paginator($adapter);
+        $paginator->setCurrentPageNumber($id)
+                ->setItemCountPerPage(1);
+        return $paginator;
+    }
 }
